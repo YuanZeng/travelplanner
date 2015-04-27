03558b19cd1635f64dd005e9554eb8e56d5a9a63
Yuan Zeng <zengyuan2010@gmail.com>
2015-04-27 15:10:39 -0400

    add user photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pins
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "30x30>" }, :default_url => "missingprofilephoto.png"
+	validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   validates :name, presence: true
+  
 end
